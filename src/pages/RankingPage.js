ebc208ceee6bfd3bb59972064435ce3d1a579488
Agustin <agustinemmanuel.ml@gmail.com>
2020-04-13 22:26:01 -0300

    Corrijo enlaces de encabezado y agrego layout en pagina de ranking

--- src/pages/RankingPage.js
+++ src/pages/RankingPage.js
@@ -3,6 +3,8 @@ import useUserData from "../hooks/useUserData";
 import Leaderboard from '../components/ranking/Leaderboard';
 import TableWithPagination from '../components/ranking/TableWithPagination';
 
+import Layout from 'components/Layout'
+
 const RankingPage = () => {
 
     const { nickname, updateNickname } = useUserData(); // Our data and methods
@@ -13,10 +15,10 @@ const RankingPage = () => {
     // }
     
     return (
-        <Fragment>
+      <Layout isAuth={true}>
             <h1>Ranking</h1>
             <Leaderboard/>
-        </Fragment>
+        </Layout>
     );
 }
 
